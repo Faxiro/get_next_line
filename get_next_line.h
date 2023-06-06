68edb0655e3dc05e2e8eacb8ce891cb5a3a1975d
Faxiro <118540211+Faxiro@users.noreply.github.com>
2023-06-06 21:08:09 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -3,19 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aviscogl <aviscogl@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: tleroy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/14 17:38:17 by aviscogl          #+#    #+#             */
-/*   Updated: 2023/03/14 17:38:17 by aviscogl         ###   ########lyon.fr   */
+/*   Created: 2023/03/30 16:58:13 by tleroy            #+#    #+#             */
+/*   Updated: 2023/03/30 17:02:55 by tleroy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 5
 # endif
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdint.h>
+
 char	*get_next_line(int fd);
+int		find_new_line(char *storage);
+char	*read_file(int fd, char *storage);
+char	*cut_line(int cut, char **storage_pt, int i);
+char	*add_to_buffer(char *buf, char *storage);
+
+/******************/
+/*     utils      */
+/******************/
+
+int		ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
