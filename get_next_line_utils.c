c07461a05361fc74e55415dfcea5ccdd5fdb013e
Faxiro <118540211+Faxiro@users.noreply.github.com>
2023-06-06 21:06:55 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,23 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aviscogl <aviscogl@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: tleroy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/17 17:24:58 by aviscogl          #+#    #+#             */
-/*   Updated: 2023/02/17 17:24:58 by aviscogl         ###   ########lyon.fr   */
+/*   Created: 2023/04/27 16:03:42 by tleroy            #+#    #+#             */
+/*   Updated: 2023/06/01 19:48:04 by tleroy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-int ft_strlen(char *s)
+int	ft_strlen(const char *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-        i++;
-    return (i);
+	i = 0;
+	if (s == NULL)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
 }
 
 char	*ft_strdup(const char *s)
@@ -28,35 +31,83 @@ char	*ft_strdup(const char *s)
 	int		size;
 	int		i;
 
-	i = -1;
+	i = 0;
 	size = ft_strlen(s);
 	dup = malloc(sizeof(char) * size + 1);
-	if (dup == 0)
-		return (0);
-	while (++i < size)
+	if (dup == NULL)
+		return (NULL);
+	while (i < size)
+	{
 		dup[i] = s[i];
-	dup[i] = 0;
+		i++;
+	}
+	dup[i] = '\0';
 	return (dup);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	j;
+	int		i;
+	int		j;
 	char	*joined;
 
-	joined = malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (joined == 0)
-		return (0);
-	i = -1;
-	while (s1[++i])
-		joined[i] = s1[i];
-	j = -1;
-	while (s2[++j])
+	if (!s1 && !s2)
+		return (NULL);
+    if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+        return (ft_strdup(s1));
+	joined = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (joined == NULL)
+		return (NULL);
+	i = 0;
+    j = 0;
+    while (s1[i])
 	{
-		joined[i] = s2[j];
+        joined[i] = s1[i];
 		i++;
 	}
-	joined[i] = '\0';
+    while (s2[j])
+	{
+		joined[i + j] = s2[j];
+		j++;
+	}
+	joined[i + j] = '\0';
 	return (joined);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*new;
+	unsigned int	i;
+	unsigned int	s_len;
+
+	if (s == 0)
+		return (0);
+	s_len = ft_strlen(s);
+	if (s_len <= start)
+		len = 0;
+	else if (s_len < start + len)
+		len = s_len - start;
+	new = malloc(sizeof(char) * (len + 1));
+
+	if (new == 0)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		new[i] = s[start + i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = -1;
+	while (++i < n)
+		((char *)s)[i] = '\0';
 }
