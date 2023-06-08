90e528ce6d7ecdb7c7dd1e6cd0e9a3385a0788ce
Faxiro <118540211+Faxiro@users.noreply.github.com>
2023-06-08 02:40:30 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -28,54 +28,6 @@ si il y a un retour a la ligne renvoyer la phrase + cut la phrase renvoyé et
 fermer le programme
 */
 
-char	*read_file(int fd, char *storage)
-{
-	char	*buf;
-	int		readed;
-	int		nl;
-
-	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (buf == NULL)
-	{
-		return (NULL);
-	}
-	readed = read(fd, buf, BUFFER_SIZE);
-	while (readed >= 0)
-	{
-		if (readed == -1 || (readed == 0 && storage[0] == '\0'))
-		{
-			free(buf);
-			free(storage);
-			return (NULL);
-		}
-		buf[readed] = '\0';
-		storage = add_to_buffer(buf, storage);
-		nl = find_new_line(storage);
-		if (nl >= 0)
-		{
-			return (free(buf), storage);
-		}
-		readed = read(fd, buf, BUFFER_SIZE);
-	}
-	return (free(buf), storage);
-}
-
-int	find_new_line(char *storage)
-{
-	int	i;
-
-	i = 0;
-	if (storage[0] == '\0')
-		return (0);
-	while (storage[i])
-	{
-		if (storage[i] == '\n')
-			return (i);
-		i++;
-	}
-	return (-1);
-}
-
 char	*add_to_buffer(char *buf, char *storage)
 {
 	char	*added;
@@ -85,41 +37,91 @@ char	*add_to_buffer(char *buf, char *storage)
 	return (added);
 }
 
-char	*cut_line(int cut, char **storage_pt, int i)
+char	*get_line(char *storage)
 {
 	char	*line;
-	char	*tempo;
+	int		i;
 	
-	if (*storage_pt == NULL)
+	i = 0;
+	while (storage[i] && storage[i] != '\n')
+		i++;
+	line = ft_calloc(i + 2, sizeof(char);
+	if (line == 0)
 		return (NULL);
-	line = malloc(sizeof(char) * cut + 2);
-	if (line == NULL)
-		return (NULL);
-	while ((*storage_pt)[i] && i <= cut)
+	i = 0;
+	while (storage[i] && storage[i] != '\n')
 	{
-		line[i] = (*storage_pt)[i];
+		line[i] = storage[i];
 		i++;
 	}
-	line[i] = '\0';
-	tempo = ft_substr(*storage_pt, cut + 1, ft_strlen(*storage_pt) - cut);
-	if (tempo == NULL)
-		return (NULL);
-	free(*storage_pt);
-	*storage_pt = ft_strdup(tempo);
-	free(tempo);
+	line[i] = '\n';
 	return (line);
+}
+
+char	*cut_line(char * storage)
+{
+	char	*new_storage;
+	int		i;
+	int		j;
+	
+	i = 0;
+	while (storage[i] && storage[i] != '\n')
+		i++;
+// 	if (storage[i] == 0)
+// 	{
+// 		free(storage);
+// 		return (NULL);
+// 	}
+	new_storage = ft_calloc(i + 1, sizeof(char));
+	if (new_storage == 0)
+		return (NULL);
+	j = 0;
+	while (storage[i])
+		new_storage[j++] = storage[i++];
+	free(storage);
+	return (new_storage);
+}
+
+char	*read_file(int fd, char *storage)
+{
+	char	*buf;
+	int		readed;
+	
+	buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (buf == NULL)
+		return (NULL);
+	readed = 1;
+	while (readed > 0)
+	{
+		readed = read(fd, buf, storage);
+		if (readed == -1)
+		{
+			free(buf);
+			free(storage);
+			return (NULL);
+		}
+		buf[readed] = '\0';
+		storage = add_to_buffer(buf, storage);
+		readed = 0;
+		while (storage[readed] && storage[readed] != '\n')
+			readed++;
+		if (storage[readed] && storage[readed] == '\n')
+			return (free(buf), storage)
+	}
+	return (free(buf), storage);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*storage = 0;
-	int			cut;
+	char		*line;
 
-	if (fd == -1 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0))
 		return (NULL);
 	storage = read_file(fd, storage);
 	if (storage == NULL)
 		return (NULL);
-	cut = find_new_line(storage);
-	return (cut_line(cut, &storage, 0));
+	line = get_line(storage);
+	storage = cut_line(storage);
+	return (line);
 }
